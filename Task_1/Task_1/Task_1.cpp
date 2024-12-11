0256916224fc82c0d3cc4643b23663a1465119fc
Nicoolay <Rozhnov.nv1992@gmail.com>
2024-12-11 07:46:50 +0700

    Добавьте файлы проекта.

---
+++ Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void print_vector(std::vector<int>& arr) {
+	for (int value : arr) {
+		std::cout << value << " ";
+}
+	std::cout << std::endl;
+}
+
+void my_sort(std::vector<int>& arr){
+	std::cout << "[IN]: ";
+	print_vector(arr);
+	std::sort(arr.begin(), arr.end());
+	auto new_end = std::unique(arr.begin(), arr.end());
+	arr.erase(new_end, arr.end());
+	std::cout << "[OUT]: ";
+	print_vector(arr);
+}
+
+int main()
+{
+	std::vector<int> arr{ 1, 1, 2, 5, 6, 1, 2, 4 };
+	my_sort(arr);
+
+
+}
---
+++ Task_2/Task_2/Task_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <list>
+#include <set>
+#include <vector>
+#include <string>
+#include <iterator>
+
+template <class T>
+void print_container(T test){
+
+	for (auto it = test.begin(); it != test.end(); ++it) {
+		std::cout << *it ;
+		if (std::next(it) != test.end()) {
+			std::cout << ", ";
+		}
+		else
+			std::cout << ". ";
+	}
+	std::cout << std::endl;
+}
+
+
+int main()
+{
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set); // four one three two. помните почему такой порядок? :)
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list); // one, two, three, four
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector); // one, two, three, four
+
+}
